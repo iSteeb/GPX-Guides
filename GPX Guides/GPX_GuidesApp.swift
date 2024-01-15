43e97b2662de40b8ed2b6af7207f1650b2aff299
Steven Duzevich <steven@duz.ie>
2024-01-15 12:54:37 +0100

    Initial Commit

---
+++ GPX Guides/GPX_GuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPX_GuidesApp.swift
+//  GPX Guides
+//
+//  Created by Steven Duzevich on 15/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct GPX_GuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPX GuidesUITests/GPX_GuidesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GPX_GuidesUITestsLaunchTests.swift
+//  GPX GuidesUITests
+//
+//  Created by Steven Duzevich on 15/1/2024.
+//
+
+import XCTest
+
+final class GPX_GuidesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
